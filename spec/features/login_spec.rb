b71f4b618a0bbaec09397ddb0232bc1355c936f0
Jay Dave <jaycdave@gmail.com>
2014-10-16 16:38:36 -0700

    login in feature test working

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,7 +1,10 @@
-# # user can get access to a login form
 
-# feature "User can log into app" do
-# 	scenario "on click user will be brought to login form" do
-# 		visit '/'
+require 'capybara/rspec'
 
-# end
+feature "User can log into app" do
+	scenario "on click user will be brought to login form" do
+		visit '/user/login'
+		expect(page).to have_content 'Login'
+	end
+end
+
