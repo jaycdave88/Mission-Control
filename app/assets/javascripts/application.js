757db0b0852286d17f19508a9e9aa5ba3f9ccecb
Joan Torres <joan.torres@upr.edu>
2014-10-20 17:17:41 -0400

    Removes js code from manifest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,16 +21,3 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
-
-$("a[href='/users/new']").on("click", function(){
-$.ajax("/users/new").done(function(data){
-     $("#SignupModal").append(data);
-   });
-})
-
-$("#sticky-list").on("click", function(e){
-  e.preventDefault();
-$.ajax(e.target.href).done(function(data){
-     $("#StickyModal").html(data);
-   });
-})
