b6cdd3b1e6c48e8e77b60c9e6f2f51de222d6ebe
Jay Dave <jaycdave@gmail.com>
2014-10-20 12:15:22 -0700

    added constrains for all models and added rspec test

--- db/migrate/20141016224940_create_stickies.rb
+++ db/migrate/20141016224940_create_stickies.rb
@@ -1,9 +1,9 @@
 class CreateStickies < ActiveRecord::Migration
   def change
     create_table :stickies do |t|
-      t.string :title
-      t.text :content
-      t.belongs_to :user
+      t.string :title, null: false
+      t.text :content,  null: false
+      t.belongs_to :user, null:false
       t.timestamps
     end
   end
--- db/migrate/20141017191714_create_helps.rb
+++ db/migrate/20141017191714_create_helps.rb
@@ -1,11 +1,11 @@
 class CreateHelps < ActiveRecord::Migration
   def change
     create_table :helps do |t|
-      t.string :title
-      t.text :content
-      t.belongs_to :user
-      t.belongs_to :sticky
-      t.timestamps
+      t.string :title,  null: false
+      t.text :content,  null: false
+      t.belongs_to :user,  null: false
+      t.belongs_to :sticky,  null: false
+      t.timestamps 
     end
   end
 end
