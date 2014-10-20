0b0ea1cd26ef141b6b6ac1a3e7061a169a1334d0
Beccs <beccs1990@gmail.com>
2014-10-20 12:13:43 -0700

    created sessions controller and implemented login into sessions controller. Added logout functionality along with it, and included flash notices in application layout.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to users_path(user.id)
+    else
+      flash[:notice] = "Invalid email or password"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged Out"
+    redirect_to root_url
+  end
+
+end
